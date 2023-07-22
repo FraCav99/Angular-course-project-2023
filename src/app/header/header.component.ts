ad0f3545173ec7ac6d617252fb738fdb89b3a3c5
Francesco <mastercaster36@outlook.it>
2023-07-22 13:10:39 +0200

    add component communication with data binding and event binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   templateUrl: './header.component.html',
@@ -7,4 +7,5 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
 
+  @Output() selectedMenuItem = new EventEmitter<string>();
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,10 +8,15 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
 
+  @Output() selectedRecipeFromList = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Test recipe', 'This is a test',
       'https://www.allrecipes.com/thmb/3x-XkV8T36df_M4tkoDbaD-WmJs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/49404-juiciest-hamburgers-ever-DDMFS-4x3-86fc27c741dd410aa365f96490c54060.jpg'
-    )
+    ),
+    new Recipe('Test recipe 2', 'This is a test',
+      'https://www.allrecipes.com/thmb/3x-XkV8T36df_M4tkoDbaD-WmJs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/49404-juiciest-hamburgers-ever-DDMFS-4x3-86fc27c741dd410aa365f96490c54060.jpg'
+    ),
   ];
 
 }
