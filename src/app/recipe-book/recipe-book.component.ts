fb62ae71c6d9eb127761c84ec0fe4f6351fde393
Francesco <mastercaster36@outlook.it>
2023-08-18 12:20:26 +0200

    implemented cross component communication with services

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,12 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { RecipeService } from "./recipe.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-recipe-book',
   styleUrls: ['./recipe-book.component.css'],
-  templateUrl: './recipe-book.component.html'
+  templateUrl: './recipe-book.component.html',
+  providers: [RecipeService]
 })
-export class RecipeBookComponent {
+export class RecipeBookComponent implements OnInit {
 
-  selectedRecipe!: Recipe;
+  selectedRecipe$!: Observable<Recipe>;
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.selectedRecipe$ = this.recipeService.selectedRecipe$;
+  }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,22 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
-  @Output() selectedRecipeFromList = new EventEmitter<Recipe>();
+  recipes!: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('Test recipe', 'This is a test',
-      'https://www.allrecipes.com/thmb/3x-XkV8T36df_M4tkoDbaD-WmJs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/49404-juiciest-hamburgers-ever-DDMFS-4x3-86fc27c741dd410aa365f96490c54060.jpg'
-    ),
-    new Recipe('Test recipe 2', 'This is a test',
-      'https://www.allrecipes.com/thmb/3x-XkV8T36df_M4tkoDbaD-WmJs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/49404-juiciest-hamburgers-ever-DDMFS-4x3-86fc27c741dd410aa365f96490c54060.jpg'
-    ),
-  ];
+  constructor(private recipeService: RecipeService) { }
 
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipeList();
+  }
 }
